82283719597e3f360fb4951434b597879f1448a0
Jagdish Parmar <prmjagdish@gmail.com>
2026-02-18 14:41:54 +0530

    feat(auth): add default ADMIN seeding on application startup.  (#2)
    * feat(auth): add admin seeder and allow admin to create admin and brand
    
    - Added AdminSeeder to run on application startup
    - Default admin created using environment variables
    - Prevents duplicate admin creation
    - Enabled ADMIN role to create ADMIN users
    - Enabled ADMIN role to create Brand
    - Applied role-based guards for protection
    
    * refactor: remove brand module and related configurations
    
    - Deleted Brand dto, entity, module, service, and controller
    - Removed brand-related migrations
    - Cleaned up role permissions and unused imports

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import { AdminSeeder } from '../../common/seeder/admin.seeder';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  providers: [UsersService, AdminSeeder],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
